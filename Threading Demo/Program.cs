c40e28c5f62e8e82eee6311baac021bcfc775645
sanjay bharathi <sanjaybharathis18087@it.ssn.edu.in>
2023-09-13 12:22:53 +0530

    Add project files.

---
+++ Threading Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+namespace  Threading
+{ 
+    class Program { 
+        static void Main() 
+        { 
+
+            Thread WorkerThread = new Thread(new ThreadStart(print));
+            WorkerThread.Start();
+
+           for (int i = 0; i<10; i++)
+            {
+                Console.WriteLine($"main Thread:{i}");
+                Thread.Sleep(1000);
+            }
+              Console.ReadKey();
+
+            }
+        static void print()
+        {
+            for (int i = 11; i < 20; i++) {
+                Console.WriteLine($"WorkerThread:{i}");
+                Thread.Sleep(1000);
+                    }
+        }
+        }
+}
