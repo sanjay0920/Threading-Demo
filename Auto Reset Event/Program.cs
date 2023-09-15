3babfa6a246ba33f72e74655b13ae5587a524abb
sanjay bharathi <sanjaybharathis18087@it.ssn.edu.in>
2023-09-15 16:07:52 +0530

    Thread Synchronization Implemented

--- Auto Reset Event/Program.cs
+++ Auto Reset Event/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AutoResetEventDemo
+{
+    internal class Program
+    {
+        static AutoResetEvent _auto = new AutoResetEvent(true);
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(Write).Start();
+            }
+           
+
+            Console.ReadKey();
+        }
+        public static void Write()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} is waiting");
+            _auto.WaitOne();
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} starts writing");
+            Thread.Sleep(1000);
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} writing completed");
+            _auto.Set();
+
+        }
+    }
+}
--- Manual Reset Event/Program.cs
+++ Manual Reset Event/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ManualResetEventdemo
+{
+    internal class Program
+    {
+        static ManualResetEvent _manual = new ManualResetEvent(false);
+        
+        static void Main(string[] args)
+        {
+            Thread t2 = new Thread(Write);
+            t2.Name = "Sanjay";
+            t2.Start();
+            for (int i = 0; i < 5; i++)
+            {
+           
+                Thread t1 = new Thread(Read);
+                t1.Name = String.Concat("Thread -", i);
+                t1.Start();
+            }
+            Console.ReadKey();
+        }
+        public static void Write()
+        {
+
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} starts writing");
+            _manual.Reset();
+            string k = Thread.CurrentThread.Name + " is reading\n";
+            File.AppendAllText("threadwriting.txt", k);
+            Thread.Sleep(1000);
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} writing completed");
+            _manual.Set();
+        }
+        public static void Read()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} waiting to Read");
+            _manual.WaitOne();
+           
+            string[] lines = File.ReadAllLines("threadwriting.txt");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Reading completed");
+         
+        }
+    }
+}
